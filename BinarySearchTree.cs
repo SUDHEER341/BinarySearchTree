b172527cc00a5d061002ad148a2a969e40b12943
SUDHEER341 <sudheer601220@gmail.com>
2023-06-07 22:40:40 +0530

    Added : Added binary search tree Add method

---
+++ BinarySearchTree.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class BinarySearchTree
+    {
+        private class Node
+        {
+            public int Data;
+            public Node Left;
+            public Node Right;
+
+            public Node(int data)
+            {
+                Data = data;
+            }
+        }
+        private Node Root;
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (Root == null)
+            {
+                Root = newNode;
+                return;
+            }
+
+            Node temp = Root;
+
+            while (temp != null)
+            {
+                if (newNode.Data < temp.Data)
+                {
+                    if (temp.Left == null)
+                    {
+                        temp.Left = newNode;
+                        return;
+                    }
+
+                    temp = temp.Left;
+                }
+                else if (newNode.Data > temp.Data)
+                {
+                    if (temp.Right == null)
+                    {
+                        temp.Right = newNode;
+                        return;
+                    }
+                    temp = temp.Right;
+                }
+                else
+                {
+                    Console.WriteLine($"{data} is already present in the tree");
+                    return;
+                }
+            }
+        }
+           
+     }
+}
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree bst = new BinarySearchTree();
+
+            bst.Add(12);
+            bst.Add(22);
+            bst.Add(45);
+            bst.Add(65);
+            bst.Add(26);
+            bst.Add(78);
+            bst.Add(20);
+            bst.Add(129);
+
+           
+
+        }
+    }
+}
